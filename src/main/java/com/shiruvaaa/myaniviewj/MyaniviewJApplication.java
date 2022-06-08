79970e92f81cafa81f9414e1f05a9c99abe1e8ad
silverfs <faizalsusetyo@gmail.com>
2022-06-08 09:37:13 +0200

    Initial Commit

---
+++ src/main/java/com/shiruvaaa/myaniviewj/MyaniviewJApplication.java
@@ -0,0 +1,13 @@
+package com.shiruvaaa.myaniviewj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyaniviewJApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyaniviewJApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shiruvaaa/myaniviewj/MyaniviewJApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shiruvaaa.myaniviewj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyaniviewJApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
